1288a054cf683fcd90ecebf735081036599577f9
david <david@PC-Uni-Satellite-Pro-U400>
2016-12-28 03:47:58 +0100

    Añadida la clase Persona

---
+++ Poo_Tr/src/Codigo/Persona.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codigo;
+
+/**
+ *
+ * @author david
+ */
+public class Persona {
+    private String nombre;
+    private String apellidos;
+    private String usuario;
+//Para darle emoción no vamos a guardar la contraseña si no un hash de la misma
+    private int password;
+    
+    public Persona (String nombre, String apellidos, String usuario,String password){
+      this.nombre = nombre;
+      this.apellidos = apellidos;
+      this.usuario = usuario;
+      this.password = password.hashCode();
+    }
+    //de momento no vamos a implementar ninguna forma de modificar usuarios
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+}
