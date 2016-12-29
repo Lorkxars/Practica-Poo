470088fcab7ea31f053f0d6dd7fc3f2eed9b144f
david <david@Tox>
2016-12-29 01:41:35 +0100

    Nuevas clases Trabajador y Dueno

---
+++ Poo_Tr/src/Codigo/Dueno.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codigo;
+
+/**
+ *
+ * @author david
+ */
+public class Dueno extends Persona{
+    public Dueno (String nombre, String apellidos, String usuario,String password){
+        super(nombre, apellidos, usuario, password);
+    }
+}
---
+++ Poo_Tr/src/Codigo/Trabajador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Codigo;
+
+/**
+ *
+ * @author david
+ */
+public class Trabajador extends Persona{
+    private double sueldo;
+    
+    public Trabajador(String nombre, String apellidos, String usuario,String password, double sueldo){
+        super(nombre, apellidos, usuario, password);
+        this.sueldo = sueldo;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+    
+}
